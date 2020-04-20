96499ce2b0419735c6c327e0a721c0208f4015af
fatalcoder524 <surajpai524@gmail.com>
2020-04-20 23:15:31 +0530

    Updates

---
+++ precache-manifest.a071ff8b13c1c93ba19ea0f2a48dee37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8011841f3a108101c36dca76ec2e97f0",
+    "url": "/testAppReact/index.html"
+  },
+  {
+    "revision": "69691036efe4eafb40e1",
+    "url": "/testAppReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8a3fedd03f5b4c8e0884",
+    "url": "/testAppReact/static/js/2.35ea2763.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testAppReact/static/js/2.35ea2763.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69691036efe4eafb40e1",
+    "url": "/testAppReact/static/js/main.fa5a67eb.chunk.js"
+  },
+  {
+    "revision": "44403d8b51c6d6c8f416",
+    "url": "/testAppReact/static/js/runtime-main.0fe88703.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testAppReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
