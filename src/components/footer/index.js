fb7a884f0db2869c30d1a57121c41bce6068b052
fatalcoder524 <surajpai524@gmail.com>
2020-04-21 14:57:52 +0530

    Fix Package.json

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,7 +7,7 @@ export default class Footer extends Component {
             <div class="fixed-bottom text-center d-flex flex-column bg-dark text-white">
             <footer class="footer">
                 <div>
-                <a href="https://www.linkedin.com/in/fatalcoder524/">
+                <a href="https://www.linkedin.com/in/fatalcoder524/" target="_blank">
                 	fatalcoder524
                 </a><span>&copy; 2020</span>
                 </div>
