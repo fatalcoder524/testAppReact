3b6e5bdd4a6f2907c963d27d3563c55acb004351
fatalcoder524 <surajpai524@gmail.com>
2020-04-21 14:48:34 +0530

    Updates

---
+++ precache-manifest.fc0f557227c5f6984f3b1269e2634ab2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcd6bcb37c5bb850175105bab9bfa34",
+    "url": "/testAppReact/index.html"
+  },
+  {
+    "revision": "bdd31b20b25aea93673e",
+    "url": "/testAppReact/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "d2387284d2d1b78c671d",
+    "url": "/testAppReact/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "bdd31b20b25aea93673e",
+    "url": "/testAppReact/static/js/2.f552eabc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/testAppReact/static/js/2.f552eabc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2387284d2d1b78c671d",
+    "url": "/testAppReact/static/js/main.fd177bbe.chunk.js"
+  },
+  {
+    "revision": "44403d8b51c6d6c8f416",
+    "url": "/testAppReact/static/js/runtime-main.0fe88703.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testAppReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
