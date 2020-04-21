5d0039e9ceac0ed5f82ce2828e7585c99e262293
fatalcoder524 <surajpai524@gmail.com>
2020-04-21 13:54:46 +0530

    Footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div class="d-flex flex-column">
+  <footer class="footer">
+    <div>
+      <a href="https://coreui.io">CoreUI</a>
+      <span>&copy; 2020 creativeLabs.</span>
+    </div>
+    <div class="ml-auto">
+      <span>Powered by</span>
+      <a href="https://coreui.io">CoreUI</a>
+    </div>
+  </footer>
+</div>
+        )
+    }
+}
