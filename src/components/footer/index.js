ad28311f5035208dfc378e5f8c16dcdfaaa2e2a9
fatalcoder524 <surajpai524@gmail.com>
2020-04-21 14:55:19 +0530

    Footer Update

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,8 +7,9 @@ export default class Footer extends Component {
             <div class="fixed-bottom text-center d-flex flex-column bg-dark text-white">
             <footer class="footer">
                 <div>
-                fatalcoder524
-                <span>&copy; 2020</span>
+                <a href="https://www.linkedin.com/in/fatalcoder524/">
+                	fatalcoder524
+                </a><span>&copy; 2020</span>
                 </div>
             </footer>
             </div>
